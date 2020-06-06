cec6c27d189c34e784695a57e6678958380ffaa5
Giacomo Lozito <giacomolozito@users.noreply.github.com>
2020-06-06 15:01:16 +0100

    Add GPLv3 license

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # borg-scripts
 
-Set of bash scripts to simplify backups with [borg](https://www.borgbackup.org/).
+Set of bash scripts to simplify backups with [borg](https://www.borgbackup.org/).  
+These are for my personal use, but shared with the open source community in case anyone finds them useful.
 
 ### Setup
 
 Set up the following directory layout for backups:
-```
+```bash
 # create backup directory
 mkdir -p /backup
 # initiate new repo, define encryption method
--- backup.sh
+++ backup.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 #
-# Giacomo.Lozito
-# simple backup script with borg backup
+# Giacomo.Lozito@gmail.com - simple backup script with borg backup
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by             
+# the Free Software Foundation, either version 3 of the License, or                                                                                                    
+# (at your option) any later version.                                                                                                                                  
+#                                                                                  
+# This program is distributed in the hope that it will be useful,                                                                                                      
+# but WITHOUT ANY WARRANTY; without even the implied warranty of                                                                                                       
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.                                                                                                                         
+#                                                                                  
+# You should have received a copy of the GNU General Public License       
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 BACKUP_ROOT=/backup
 BACKUP_REPO=${BACKUP_ROOT}/repo
@@ -9,7 +22,7 @@ BACKUP_TOOLSDIR=${BACKUP_ROOT}/borg-scripts
 BACKUP_LOG=${BACKUP_TOOLSDIR}/backup.log
 BACKUP_CFG=${BACKUP_TOOLSDIR}/backup.sh.cfg
 
-# redirect standard output and eVrror to log
+# redirect standard output and error to log
 touch ${BACKUP_LOG}
 exec 1>>${BACKUP_LOG}
 exec 2>&1
